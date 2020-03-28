240a7e28777fbddaf20037479d26c2d526a9c714
Heather Moreira <moreiraheather@gmail.com>
2020-03-28 05:49:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -16,3 +16,19 @@ def prime?(integer)
   end
 end 
   
+  
+  
+# def prime?(integer)
+#   result = true 
+#   denominators = (2..integer - 1).to_a 
+#   if integer < 2 
+#     result = false 
+#   else 
+#     denominators.each do |denominator|
+#       if integer % denominator == 0 
+#         result = false 
+#       end 
+#     end 
+#   end
+#   return result 
+# end
