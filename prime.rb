dde1548cd00f0dcd1072053f507ebbac27eda86d
Evan Smith <professionalemail@zoho.com>
2020-04-02 19:47:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+    else
+    array = (2..number-1).to_a
+    array.none? do |factor|
+    number % factor == 0
+    end
+  end
+end
