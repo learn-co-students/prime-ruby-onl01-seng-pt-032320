a83166b8dbbefc7ea4d435113b57c5ba2a50d5f7
bruce willis <fashun_vyctum@yahoo.com>
2020-04-03 04:27:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  first = 2
+  if num > 1
+  range = (first..num-1).to_a
+  range.none? do |num_to_test|
+    num % num_to_test == 0
+  end
+else
+  FALSE
+end
+end
