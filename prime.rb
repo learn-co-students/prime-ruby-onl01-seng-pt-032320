e7b3d8d705aaada3f1b1e91bfbd6023cd131d6ea
Dina Kendle <dina.kendle@gmail.com>
2020-04-03 07:48:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+ prime_int = []
+  if value <= 1 || value == 0 || value == 1
+      false
+    
+  elsif
+    (2..value - 1).each do |i|
+      prime_int <<
+      if value % i == 0
+      false
+      end
+  end
+  prime_int
+end
+end
+
