74a1d5a2e0519b8df8e7f80f407b35ac38fc661e
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-03 18:33:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number < 0 || number == 0 || number == 1
+    return false
+  else
+    (2..number-1).to_a.all? do |viable_number|
+      number % viable_number != 0
+    end
+  end
+end
+
+
+
