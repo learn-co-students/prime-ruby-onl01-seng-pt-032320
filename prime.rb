255c1169f64b9d7077a23a151aa8bdbad7d709c2
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 06:54:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(integer)
+  return true if integer==2
+  return false if integer<2
+  newarray=(2..(integer-1)).to_a
+  newarray.none? do |x|
+    integer%x==0
+  end
+end
